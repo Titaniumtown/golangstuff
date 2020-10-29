d6a4cce46bd456fe635399ac0fdeac78310c33a3
Simon Gardling <titaniumtown@gmail.com>
2020-10-29 11:29:59 -0400

    print in console when commands are being run

--- discord-bot/main.go
+++ discord-bot/main.go
@@ -88,8 +88,10 @@ func ComesFromDM(s *discordgo.Session, m *discordgo.MessageCreate) (bool, error)
 func ownertest(s *discordgo.Session, m *discordgo.MessageCreate, owner_id string) {
 	if m.Author.ID == owner_id {
 		s.ChannelMessageSend(m.ChannelID, "you own the bot")
+		fmt.Println("User is an owner")
 	} else {
 		s.ChannelMessageSend(m.ChannelID, "you don't own the bot")
+		fmt.Println("Use isn't an owner")
 	}
 
 }
@@ -142,6 +144,7 @@ func uptime(s *discordgo.Session, m *discordgo.MessageCreate) {
 		fmt.Println(error_str)
 		codesnippetprint(s, m, error_str)
 	}
+	fmt.Println("ran uptime")
 
 }
 
@@ -166,12 +169,12 @@ func temps(s *discordgo.Session, m *discordgo.MessageCreate) {
 		fmt.Println(error_str)
 		codesnippetprint(s, m, error_str)
 	}
+	fmt.Println("ran temps")
 }
 
 // bash stuff, bc why not?
 func bashRun(s *discordgo.Session, m *discordgo.MessageCreate) {
 	cmdstring := strings.Replace(m.Content, "!bash ", "", -1)
-
 	cmd := exec.Command("sudo", "su", "discord", "bash", "-c", cmdstring)
 
 	fmt.Println("running bash command:", cmdstring)
@@ -179,6 +182,7 @@ func bashRun(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	if uniseg.GraphemeClusterCount(string(out)) > 2000 {
 		s.ChannelMessageSend(m.ChannelID, "Sorry, the output of that command is over the 2000 character limit set by discord")
+		fmt.Println("Message sent was over 2000 char limit")
 		return
 	}
 
@@ -195,52 +199,65 @@ func bashRun(s *discordgo.Session, m *discordgo.MessageCreate) {
 func noPermsCmd(s *discordgo.Session, m *discordgo.MessageCreate, owner_id string) {
 	switch m.Content {
 	case "!ping":
+		fmt.Println("running ping")
 		s.ChannelMessageSend(m.ChannelID, "pong")
 
 	case "!userid":
+		fmt.Println("running userid")
 		// prints the id of the user doing !userid
 		s.ChannelMessageSend(m.ChannelID, m.Author.ID)
 
 	case "!ownertest":
+		fmt.Println("Running ownertest")
 		ownertest(s, m, owner_id)
 
 	case "!dmtest":
+		fmt.Println("running dmtest")
 		dmtest(s, m)
 
 	case "!jebaited":
+		fmt.Println("running jebaited")
 		// you just got jebaited
 		s.ChannelMessageSend(m.ChannelID, "http://www.gardling.com/coolvideo6")
 
 	case "!whyiuselinux":
+		fmt.Println("running whyiuselinux")
 		s.ChannelMessageSend(m.ChannelID, "http://www.gardling.com/whyiuselinux")
 
 	case "bruh":
+		fmt.Println("responding to 'bruh' with 'moment'")
 		// responds to every message "bruh" with "moment"
 		s.ChannelMessageSend(m.ChannelID, "moment")
 
 	case "!website":
+		fmt.Println("Linking to my website")
 		// links to my website
 		s.ChannelMessageSend(m.ChannelID, "<http://www.gardling.com>")
 
 	case "thx bot":
+		fmt.Println("responding to 'thx bot' with 'np bro'")
 		// takes complements
 		s.ChannelMessageSend(m.ChannelID, "np bro")
 
 	case "pog":
+		fmt.Println("responding to 'pog' with 'poggers'")
 		// responds to "pog" with "poggers"
 		s.ChannelMessageSend(m.ChannelID, "poggers")
 
 	case "!github":
+		fmt.Println("Linking to the sourcecode of the bot")
 		// links to the github of the bot
 		s.ChannelMessageSend(m.ChannelID, "<https://github.com/Titaniumtown/golangstuff/tree/master/discord-bot>")
 
 	case "!crab":
+		fmt.Println("crab rave time")
 		// crab rave
 		s.ChannelMessageSend(m.ChannelID, ":crab: :crab: :crab: :crab: :crab: :crab: :crab:")
 		s.ChannelMessageSend(m.ChannelID, "https://www.youtube.com/watch?v=LDU_Txk06tM")
 
 	case "yeet":
-		//test
+		fmt.Println("responding to 'yeet' with 'itayayita'")
+		//test by a friend
 		s.ChannelMessageSend(m.ChannelID, "itayayita")
 		
 	default:
