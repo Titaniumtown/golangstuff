3039a6750b839951900895440fd1636c9dbcbdb4
Simon Gardling <titaniumtown@gmail.com>
2020-11-03 15:24:53 -0500

    make code more streamlined

--- discord-bot/main.go
+++ discord-bot/main.go
@@ -270,17 +270,26 @@ func noPermsCmd(s *discordgo.Session, m *discordgo.MessageCreate, owner_id strin
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == "721344385253507102" {
-		if strings.ToLower(m.Content) == strings.ToLower("gf") {
-			fmt.Println("lol deleting message saying 'gf'")
-			s.ChannelMessageDelete(m.ChannelID, m.ID)
-		}
-	}
+	// if m.Author.ID == "721344385253507102" {
+	// 	if strings.ToLower(m.Content) == strings.ToLower("gf") {
+	// 		fmt.Println("lol deleting message saying 'gf'")
+	// 		s.ChannelMessageDelete(m.ChannelID, m.ID)
+	// 	}
+	// }
 	// if message sender is the bot, ignore message
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
+	// if m.Author.ID == s.State.User.ID {
+		// return
+	// }
 
+	switch m.Author.ID{
+		case s.State.User.ID:
+			return
+		case "721344385253507102":
+			if strings.ToLower(m.Content) == strings.ToLower("gf") {
+				fmt.Println("lol deleting message saying 'gf'")
+				s.ChannelMessageDelete(m.ChannelID, m.ID)
+			}
+	}
 	// my userid: 321028131982934017
 	var owner_id = "321028131982934017"
 
