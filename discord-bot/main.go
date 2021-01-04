6c3a2a3524155738928bf4923d41d4f4c192c469
Simon Gardling <titaniumtown@gmail.com>
2021-01-04 11:56:40 -0500

    that should be a string

--- discord-bot/main.go
+++ discord-bot/main.go
@@ -313,7 +313,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 			return
 		default:
 			printcreate(s,m)
-			if (m.GuildID == 795029627750973512 && m.Author.String() == "GitHub#0000") || (m.Content == "!githubnotificationtest") {
+			if (m.GuildID == "795029627750973512" && m.Author.String() == "GitHub#0000") || (m.Content == "!githubnotificationtest") {
 				s.ChannelMessageSend(m.ChannelID, "@github notifications new commits pushed to TitaniumMC! Get the latest build from: <http://www.gardling.com/titaniumclip.jar>")
 			}
 			if m.Author.ID == owner_id {
